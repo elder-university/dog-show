56984e22464d8ac7142f4d1f504254614120b820
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2020-10-22 18:06:08 +0100

    Fix routing for Laravel 8, install factory legacy package as quick fix

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,25 @@
 <?php
 
+use App\Http\Controllers\DogController;
+use App\Http\Controllers\MailController;
+
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
 Route::middleware ('auth') -> group (function () {
 
-    Route::get ('/', 'DogController@index') -> name ('index');
+    Route::get ('/', [DogController::class, 'index']) -> name ('index');
 
-    Route::get ('/dog/{dog}/', 'DogController@show');
-    Route::delete ('/dog/{dog}/', 'DogController@destroy');
+    Route::get ('/dog/{dog}/', [DogController::class, 'show']);
+    Route::delete ('/dog/{dog}/', [DogController::class, 'destroy']);
 
-    Route::get ('/dog/', 'DogController@create');
-    Route::post ('/dog/', 'DogController@store');
+    Route::get ('/dog/', [DogController::class, 'create']);
+    Route::post ('/dog/', [DogController::class, 'store']);
 
-    Route::get ('/dog/{dog}/edit', 'DogController@edit');
-    Route::patch ('/dog/{dog}/', 'DogController@update');
+    Route::get ('/dog/{dog}/edit', [DogController::class, 'edit']);
+    Route::patch ('/dog/{dog}/', [DogController::class, 'update']);
 
-    Route::get ('/dog/{dog}/email', 'MailController@sendDogDetails');
+    Route::get ('/dog/{dog}/email', [MailController::class, 'sendDogDetails']);
 
 });
